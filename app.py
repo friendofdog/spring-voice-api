eb831c1ae7d5cf8c5e29ac89c9c864caa2e2d297
Kevin Kee <kevinkee9@gmail.com>
2020-07-11 11:06:42 +0900

    Test: Add Flake8 and add to Test script; fix related error in activate
    Only ignored directories are venv and __pycache__. Check that there are
    no others which should be ignored.

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 VERSION = "v1"
 
+
 @app.route(f"/api/{VERSION}/healthcheck", methods=['GET'])
 def healthcheck():
     try:
         return jsonify({"success": True}), 200
     except Exception as e:
         return f"An error occured: {e}"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
