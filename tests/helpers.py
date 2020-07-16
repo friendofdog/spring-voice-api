e894c3ef974a747d7eb1f43cb323f11717f70e4b
Kevin Kee <kevinkee9@gmail.com>
2020-07-16 21:21:29 +0900

    tests/helpers: Replace test_app_dev with decorators; rename test_client
    test_app_dev() had been modifying sys.argv directly instead of mocking.
    Replaced with decorators in order to mock propertly. test_client() ->
    make_test_client() because it was confusing pytest.
    
    resolving #5

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,17 +1,9 @@
 import contextlib
-import sys
-
 from springapi.app import create_app
 
 
-def test_app_dev():
-    sys.argv.append('--env=test')
-    app = create_app()
-    return app
-
-
 @contextlib.contextmanager
-def test_client():
-    app = test_app_dev()
+def make_test_client():
+    app = create_app()
     with app.test_client() as client:
         yield client
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,18 @@
-from springapi.app import get_env_name
-from tests.helpers import test_app_dev
+from springapi.app import get_env_name, create_app
 import unittest
+from unittest.mock import patch
+import sys
 
 
 class TestAppConfig(unittest.TestCase):
 
+    @patch('sys.argv', [sys.argv[0], '--env=test'])
     def test_config_attr_testing_true(self):
-        app = test_app_dev()
+        app = create_app()
         app_env = app.config
         self.assertEqual(app_env['TESTING'], True)
 
+    @patch('sys.argv', [sys.argv[0], '--env=test'])
     def test_development_environment(self):
         env_name = get_env_name()
         self.assertEqual(env_name, 'TestingConfig')
