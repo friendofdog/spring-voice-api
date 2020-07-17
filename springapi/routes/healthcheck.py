728c3fdd2a230ebcb8814a6174d8d606c9270d28
Kevin Kee <kevinkee9@gmail.com>
2020-07-17 15:55:07 +0900

    healthcheck: Remove try/except

--- springapi/routes/healthcheck.py
+++ springapi/routes/healthcheck.py
@@ -1,10 +1,6 @@
 from springapi.helpers import route, VERSION
-from flask import jsonify
 
 
 @route(f"/api/{VERSION}/healthcheck", methods=['GET'])
 def healthcheck():
-    try:
-        return jsonify({"success": True}), 200
-    except Exception as e:
-        return f"An error occured: {e}"
+    return {"success": True}, 200
