9b31594a3f1ffb789f6393d0978ffdf2823d5420
Kevin Kee <kevinkee9@gmail.com>
2020-08-01 17:20:25 +0900

    database: Add create_submission() with data validation and tests

--- springapi/models/db.py
+++ springapi/models/db.py
@@ -1,5 +1,55 @@
 import springapi.models.firebase.client as client
 
 
+def check_required_fields(fields, data):
+    required = [f for f in fields if fields[f]['isRequired'] is True]
+    missing = sorted(list(set(required) - set(list(data.keys()))))
+    return missing
+
+
+def check_type(fields, data):
+    bad_types = sorted([d for d in data if d in fields.keys() and
+                        type(data[d]) is not fields[d]['type']])
+    return bad_types
+
+
 def get_submissions():
     return client.get_collection('submissions')
+
+
+def get_submission(entry_id):
+    submission = client.get_entry('submissions', entry_id)
+    status = '200 OK' if submission else '404 NOT FOUND'
+    response = submission if submission\
+        else f'Entry with id {entry_id} not found in submissions'
+    return response, status
+
+
+def create_submission(data):
+    fields = {
+        'name': {'isRequired': True, 'type': str},
+        'message': {'isRequired': True, 'type': str},
+        'location': {'isRequired': True, 'type': str}
+    }
+
+    missing = check_required_fields(fields, data)
+    if missing:
+        return f'Missing: {", ".join(missing)}', '400 BAD REQUEST'
+
+    bad_types = check_type(fields, data)
+    if bad_types:
+        type_errors = [f'{e} is {type(data[e]).__name__} should be '
+                       f'{fields[e]["type"].__name__}'
+                       for e in bad_types]
+        return f'Fields with type errors: {", ".join(type_errors)}', \
+               '400 BAD REQUEST'
+
+    response, status = client.add_entry('submission', data)
+    if response and status == '201 CREATED':
+        return response, status
+    else:
+        return 'A server error occured', '500 INTERNAL SERVER ERROR'
+
+
+def update_submission():
+    pass
--- tests/models/test_db.py
+++ tests/models/test_db.py
@@ -1,7 +1,45 @@
 import unittest
-from unittest import mock
+from mockfirestore import MockFirestore  # type: ignore
+from springapi.models.db import \
+    check_required_fields,\
+    check_type,\
+    get_submissions,\
+    get_submission,\
+    create_submission
 from tests.helpers import populate_mock_firestore_submissions
-from springapi.models.db import get_submissions
+from unittest import mock
+
+
+class TestDatabaseValidationCalls(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(TestDatabaseValidationCalls, self).__init__(*args, **kwargs)
+        self.fields = {
+            'name': {'isRequired': True, 'type': str},
+            'message': {'isRequired': True, 'type': str},
+            'age': {'isRequired': False, 'type': int},
+            'location': {'isRequired': False, 'type': str}
+        }
+
+    def test_check_required_fields_returns_missing_fields(self):
+        data = {'name': 'This Person', 'age': 33}
+        missing = check_required_fields(self.fields, data)
+        self.assertEqual(missing, ['message'])
+
+    def test_check_required_fields_returns_empty_if_no_missing(self):
+        data = {'name': 'This Person', 'message': 'hello', 'age': 33}
+        missing = check_required_fields(self.fields, data)
+        self.assertFalse(missing)
+
+    def test_check_type_returns_bad_types(self):
+        data = {'name': 10, 'message': 'hello'}
+        bad_types = check_type(self.fields, data)
+        self.assertEqual(bad_types, ['name'])
+
+    def test_check_type_returns_empty_if_types_good(self):
+        data = {'name': 'This Person', 'message': 'hello'}
+        bad_types = check_type(self.fields, data)
+        self.assertEqual(bad_types, [])
 
 
 class TestDatabaseCalls(unittest.TestCase):
@@ -15,7 +53,72 @@ class TestDatabaseCalls(unittest.TestCase):
 
     @mock.patch('springapi.models.firebase.client.firestore.client')
     def test_get_submissions_returns_empty_if_none_found(self, mocked):
+        mocked.return_value = MockFirestore()
+
+        response = get_submissions()
+        self.assertFalse(response)
+
+    @mock.patch('springapi.models.firebase.client.firestore.client')
+    def test_get_submissions_returns_list_if_found(self, mocked):
         mocked.return_value = populate_mock_firestore_submissions(self.entries)
 
         response = get_submissions()
-        self.assertEqual(response['1'], self.entries['1'])
+        self.assertEqual(response, self.entries)
+
+    @mock.patch('springapi.models.firebase.client.firestore.client')
+    def test_get_submission_returns_404_if_not_found(self, mocked):
+        mocked.return_value = populate_mock_firestore_submissions(self.entries)
+
+        entry_id = 'missing-entry'
+        not_found = f'Entry with id {entry_id} not found in submissions'
+
+        response, status = get_submission(entry_id)
+        self.assertEqual(status, '404 NOT FOUND')
+        self.assertEqual(response, not_found)
+
+    @mock.patch('springapi.models.firebase.client.firestore.client')
+    def test_get_submission_returns_submission_if_found(self, mocked):
+        mocked.return_value = populate_mock_firestore_submissions(self.entries)
+
+        response, status = get_submission('1')
+        self.assertEqual(status, '200 OK')
+        self.assertEqual(response, self.entries['1'])
+
+    @mock.patch('springapi.models.db.check_type')
+    @mock.patch('springapi.models.db.check_required_fields')
+    def test_create_submission_returns_missing_field_error(
+            self, mock_type, mock_req):
+        mock_type.return_value = ['some_field', 'another_field']
+        mock_req.return_value = []
+
+        data = {'name': 'This Person'}
+        response, status = create_submission(data)
+        self.assertEqual(response, 'Missing: some_field, another_field')
+        self.assertEqual(status, '400 BAD REQUEST')
+
+    @mock.patch('springapi.models.db.check_type')
+    @mock.patch('springapi.models.db.check_required_fields')
+    def test_create_submission_returns_type_check_error(
+            self, mock_req, mock_type):
+        mock_type.return_value = ['name']
+        mock_req.return_value = []
+
+        data = {'name': 10, 'message': 'hello'}
+        response, status = create_submission(data)
+        self.assertEqual(response, f'Fields with type errors: name is int '
+                                   f'should be str')
+        self.assertEqual(status, '400 BAD REQUEST')
+
+    @mock.patch('springapi.models.db.check_type')
+    @mock.patch('springapi.models.db.check_required_fields')
+    @mock.patch('springapi.models.firebase.client.firestore.client')
+    def test_create_submission_succeeds_if_checks_passed(
+            self, mock_client, mock_req, mock_type):
+        mock_type.return_value = []
+        mock_req.return_value = []
+        mock_client.return_value = MockFirestore()
+
+        data = {'name': 'This Person'}
+        response, status = create_submission(data)
+        self.assertEqual(response, data)
+        self.assertEqual(status, '201 CREATED')
