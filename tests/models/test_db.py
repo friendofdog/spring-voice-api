59c7980413d7f2e8ae5b068b399870802e2f1ac4
Kevin Kee <kevinkee9@gmail.com>
2020-08-03 11:05:36 +0900

    database: Remove client from tests, instantiate Submission in __init__
    Some database tests had previously been testing against a mock client.
    Removed all instances of this, replaced with mock of related methods.
    
    Instantiate Submission in __init__ of test classes instead of multiple
    times throughout test.

--- tests/models/test_db.py
+++ tests/models/test_db.py
@@ -1,70 +1,59 @@
 import unittest
-from mockfirestore import MockFirestore  # type: ignore
 from springapi.models.db import Submission, get_submission, get_submissions
-from tests.helpers import populate_mock_submissions
 from unittest import mock
 
 
 class TestDataValidation(unittest.TestCase):
 
-    def test_check_required_fields_returns_missing_fields(self):
-        submission = Submission()
+    def __init__(self, *args, **kwargs):
+        super(TestDataValidation, self).__init__(*args, **kwargs)
+        self.submission = Submission()
 
+    def test_check_required_fields_returns_missing_fields(self):
         data = {'name': 'This Person', 'message': 'hello'}
-        missing = submission._check_required_fields(data)
+        missing = self.submission._check_required_fields(data)
         self.assertEqual(missing, ['location'])
 
     def test_check_required_fields_returns_empty_if_no_missing(self):
-        submission = Submission()
-
         data = {'name': 'This Person', 'message': 'hello', 'location': 'here'}
-        missing = submission._check_required_fields(data)
+        missing = self.submission._check_required_fields(data)
         self.assertFalse(missing)
 
     def test_check_type_returns_bad_types(self):
-        submission = Submission()
-
         data = {'name': 10, 'message': 'hello'}
-        bad_types = submission._check_type(data)
+        bad_types = self.submission._check_type(data)
         self.assertEqual(bad_types, ['name'])
 
     def test_check_type_returns_empty_if_types_good(self):
-        submission = Submission()
-
         data = {'name': 'This Person', 'message': 'hello'}
-        bad_types = submission._check_type(data)
+        bad_types = self.submission._check_type(data)
         self.assertEqual(bad_types, [])
 
     @mock.patch('springapi.models.db.Submission._check_type')
     @mock.patch('springapi.models.db.Submission._check_required_fields')
     def test_validate_data_returns_missing_field_error_message(
             self, mock_type, mock_req):
-        mock_type.return_value = ['some_field', 'another_field']
+        fields = mock_type.return_value = ['some_field', 'another_field']
         mock_req.return_value = []
 
-        submission = Submission()
-
-        data = {'name': 'This Person'}
-        response = submission._validate_data(data)
-        self.assertEqual(response[0], 'Missing: some_field, another_field')
+        response = self.submission._validate_data({})
+        self.assertEqual(response[0], f'Missing: {", ".join(fields)}')
 
     @mock.patch('springapi.models.db.Submission._check_type')
     @mock.patch('springapi.models.db.Submission._check_required_fields')
     def test_validate_data_returns_type_check_error_message(
             self, mock_type, mock_req):
+        data = {'name': 10, 'message': 'hello'}
+
         mock_type.return_value = []
         mock_req.return_value = ['name']
 
-        submission = Submission()
-
-        data = {'name': 10, 'message': 'hello'}
-        response = submission._validate_data(data)
+        response = self.submission._validate_data(data)
         self.assertEqual(
             response[0], 'Fields with type errors: name is int, should be str.'
         )
 
 
-@mock.patch('springapi.models.firebase.client.firestore.client')
 class TestDatabaseCalls(unittest.TestCase):
 
     def __init__(self, *args, **kwargs):
@@ -73,69 +62,74 @@ class TestDatabaseCalls(unittest.TestCase):
             "1": {"name": "Some Guy", "message": "Hi there"},
             "2": {"name": "Another Fellow", "message": "Goodbye"}
         }
+        self.submission = Submission()
 
-    def test_get_submissions_returns_empty_if_none_found(self, mock_client):
-        mock_client.return_value = MockFirestore()
+    @mock.patch('springapi.models.firebase.client.get_collection')
+    def test_get_submissions_returns_empty_if_none_found(self, mock_get):
+        mock_get.return_value = []
 
         response = get_submissions()
         self.assertFalse(response)
 
-    def test_get_submissions_returns_list_if_found(self, mock_client):
-        mock_client.return_value = populate_mock_submissions(self.entries)
+    @mock.patch('springapi.models.firebase.client.get_collection')
+    def test_get_submissions_returns_list_if_found(self, mock_get):
+        mock_get.return_value = self.entries
 
         response = get_submissions()
         self.assertEqual(response, self.entries)
 
-    def test_get_submission_returns_404_if_not_found(self, mock_client):
-        mock_client.return_value = populate_mock_submissions(self.entries)
-
+    @mock.patch('springapi.models.firebase.client.get_entry')
+    def test_get_submission_returns_404_if_not_found(self, mock_get):
         entry_id = 'missing-entry'
-        not_found = f'Entry with id {entry_id} not found in submissions'
+
+        mock_get.return_value = {}
 
         response, status = get_submission(entry_id)
         self.assertEqual(status, '404 NOT FOUND')
-        self.assertEqual(response, not_found)
+        self.assertEqual(response, f'Entry with id {entry_id} '
+                                   f'not found in submissions')
 
-    def test_get_submission_returns_submission_if_found(self, mock_client):
-        mock_client.return_value = populate_mock_submissions(self.entries)
+    @mock.patch('springapi.models.firebase.client.get_entry')
+    def test_get_submission_returns_submission_if_found(self, mock_get):
+        entry_id = '1'
 
-        response, status = get_submission('1')
+        mock_get.return_value = self.entries[entry_id]
+
+        response, status = get_submission(entry_id)
         self.assertEqual(status, '200 OK')
-        self.assertEqual(response, self.entries['1'])
+        self.assertEqual(response, self.entries[entry_id])
 
     @mock.patch('springapi.models.db.Submission._validate_data')
     def test_create_submission_fails_if_validation_checks_fail(
-            self, mock_validation, __):
+            self, mock_validation):
         error = mock_validation.return_value = ['some error occured']
 
-        submission = Submission()
-
-        response, status = submission.create_submission({})
+        response, status = self.submission.create_submission({})
         self.assertEqual(response, error[0])
         self.assertEqual(status, '400 BAD REQUEST')
-
-    @mock.patch('springapi.models.db.Submission._validate_data')
-    def test_create_submission_succeeds_if_validation_checks_pass(
-            self, mock_validation, __):
-        mock_validation.return_value = []
-
-        submission = Submission()
-
-        data = {'name': 'This Person'}
-        response, status = submission.create_submission(data)
-        self.assertEqual(response, data)
-        self.assertEqual(status, '201 CREATED')
 
     @mock.patch('springapi.models.firebase.client.add_entry')
     @mock.patch('springapi.models.db.Submission._validate_data')
     def test_create_submission_returns_server_error_message(
-            self, mock_validation, mock_add, __):
+            self, mock_validation, mock_add):
         mock_validation.return_value = []
         error = mock_add.return_value = ['some error happened', '500']
 
-        submission = Submission()
-
-        response, status = submission.create_submission({})
+        response, status = self.submission.create_submission({})
         self.assertEqual('An error occured:\r\n'
                          f'{error[0]}', response)
         self.assertEqual(status, error[1])
+
+    @mock.patch('springapi.models.firebase.client.add_entry')
+    @mock.patch('springapi.models.db.Submission._validate_data')
+    def test_create_submission_succeeds_if_validation_checks_pass(
+            self, mock_validation, mock_add):
+        data = {'name': 'This Person'}
+        expected_status = '201 CREATED'
+
+        mock_validation.return_value = []
+        mock_add.return_value = [data, expected_status]
+
+        response, status = self.submission.create_submission(data)
+        self.assertEqual(response, data)
+        self.assertEqual(status, expected_status)
