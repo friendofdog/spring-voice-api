98073526e3e7a1c80801a04ead678eccfa58a921
Kevin Kee <kevinkee9@gmail.com>
2020-08-24 19:37:45 +0900

    springapi.app: Add tests for create_database_instance()
    Not quite TDD, but some things were moved to this method in previous
    commit and they needed testing.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,6 @@
-from springapi.app import create_app
 import unittest
+from springapi.app import create_app, create_database_instance
+from unittest import mock
 
 
 class TestSpringapiAppCreation(unittest.TestCase):
@@ -14,3 +15,23 @@ class TestSpringapiAppCreation(unittest.TestCase):
         app_env = app.config
         self.assertEqual(app_env['ENV'], 'development')
         self.assertEqual(app_env['DEBUG'], True)
+
+    def test_create_database_instance_returns_ValueError_on_bad_scheme(self):
+        scheme = 'badscheme'
+        config = {'DATABASE_URI': f'{scheme}://ImFiY2RlIg=='}
+
+        with self.assertRaises(ValueError) as context:
+            create_database_instance(config)
+
+        self.assertEqual(context.exception.__str__(),
+                         f'Unknown database protocol: {scheme}')
+
+    @mock.patch('springapi.app.authenticate')
+    def test_create_database_instance_auth_if_scheme_found(self, mocked):
+        auth = mocked.return_value = 'abc'
+
+        scheme = 'firestore'
+        config = {'DATABASE_URI': f'{scheme}://ImFiY2RlIg=='}
+
+        response = create_database_instance(config)
+        self.assertEqual(response, auth)
