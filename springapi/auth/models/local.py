43e27ce8c8bfcd277c449eb5269cac13c16cd9db
Kevin Kee <kevinkee9@gmail.com>
2020-10-19 19:33:14 +0900

    auth: Add local server to models
    Local server for testing implementation.

---
+++ springapi/auth/models/local.py
@@ -0,0 +1,30 @@
+import requests
+
+
+def request_auth_code(params=None):
+    if params is None:
+        params = {
+            "client_id": "abcde",
+            "redirect_uri": "http://localhost:5000/api/v1/auth",
+            "response_type": "code",
+            "scope": "https://www.googleapis.com/auth/userinfo.email "
+                     "https://www.googleapis.com/auth/userinfo.profile "
+                     "openid"
+        }
+    requests.get(
+        url="http://localhost:8081/mock-auth", params=params)
+    return {"success": True}
+
+
+def exchange_token(params=None):
+    if params is None:
+        params = {
+            "code": "54321",
+            "client_id": "abcde",
+            "client_secret": "qwerty",
+            "redirect_uri": "http://localhost:5000/api/v1/token",
+            "grant_type": "authorization_code"
+        }
+    requests.post(
+        url="http://localhost:8081/mock-token", params=params)
+    return {"success": True}
---
+++ tests/auth/models/test_local.py
@@ -0,0 +1,20 @@
+import requests
+import unittest
+from springapi.auth.models.local import (
+    request_auth_code, exchange_token)
+from unittest import mock
+
+
+class TestGoogleAuth(unittest.TestCase):
+
+    @mock.patch.object(requests, "get")
+    def test_request_auth_code_returns_code(self, mock_get):
+        mock_get.return_value.status_code = 200
+        response = request_auth_code({"a": "b"})
+        self.assertEqual(response, {"success": True})
+
+    @mock.patch.object(requests, "post")
+    def test_exchange_token_returns_token(self, mock_post):
+        mock_post.return_value.status_code = 200
+        response = exchange_token('some_auth_code')
+        self.assertEqual(response, {"success": True})
